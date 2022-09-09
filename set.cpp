b3afe7e4c1f1425e1b67484b42fe5b2d79e04555
Hemant Chaubey <89316018+imhemantchaubey@users.noreply.github.com>
2022-09-10 01:44:09 +0530

    stl set

---
+++ set.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <set>
+
+using namespace std;
+int main()
+{
+  set<int> s;
+
+  s.insert(5);
+  s.insert(5);
+  s.insert(5);
+  s.insert(1);
+  s.insert(6);
+  s.insert(6);
+  s.insert(0);
+  s.insert(0);
+  s.insert(0);
+
+  cout << "set s -> ";
+  for (auto i : s)
+  {
+    cout << i << " ";
+  } cout << endl;
+
+  set<int>::iterator it = s.begin();
+  it++;
+
+  s.erase(it);
+
+  for (auto i : s)
+  {
+    cout << i << " ";
+  } cout << endl;
+  cout << "Is -5 present in the set: " << s.count(-5) << endl;
+  cout << "Is 5 present in the set: " << s.count(5) << endl;
+
+  set<int>::iterator itr = s.find(5);
+
+  for (auto it = itr; it != s.end(); it++)
+  {
+    cout << *it << " ";
+  } cout << endl;
+}
